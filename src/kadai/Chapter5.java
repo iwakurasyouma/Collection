e7fb8588f8dfd0be6db12ced104042d6e5f212b8
iwakurasyouma <104047126+iwakurasyouma@users.noreply.github.com>
2022-08-11 19:56:39 +0900

    課題提出

---
+++ src/kadai/Chapter5.java
@@ -0,0 +1,21 @@
+package kadai;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Chapter5 {
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		list.add("2021-10-21" + " 牛乳を買う");
+		list.add("2021-09-15" + " ○○社面談");
+		list.add("2021-12-04" + " 手帳を買う");
+		list.add("2021-08-10" + " 散髪に行く");
+		list.add("2021-11-09" + " スクールの課題を解く");
+		Collections.sort(list);
+		for (String s : list) {
+			System.out.println(s);
+		}
+	}
+}
